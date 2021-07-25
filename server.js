279ef7c4ce73c8e331082e88b0ad50a17494bba2
Anurag Sinha <76554617+CICADA-dev64bit@users.noreply.github.com>
2021-07-25 17:55:58 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,47 @@
+// Import dependencies
+const express = require("express");
+const app = express();
+
+const hostname = '0.0.0.0'
+const port = 80;
+
+
+app.use(express.static("client"));
+
+const server = app.listen(port, hostname, () => console.log(`Listening at http://${hostname}:${port}`));
+const io = require("socket.io")(server);
+
+
+const candidates = {
+    "0": { votes: 0, label: "JavaScript", color: randomRGB() },
+    "1": { votes: 0, label: "C#", color: randomRGB() },
+    "2": { votes: 0, label: "PHP", color: randomRGB() },
+    "3": { votes: 0, label: "Python", color: randomRGB() },
+    "4": { votes: 0, label: "Go", color: randomRGB() }
+};
+
+
+io.on("connection", (socket) => {
+    io.emit("update", candidates);
+
+    
+    socket.on("vote", (index) => {
+
+       
+        if (candidates[index]) {
+            candidates[index].votes += 1;
+        }
+
+        
+        console.log(candidates);
+
+        
+        io.emit("update", candidates);
+    });
+});
+
+
+function randomRGB() {
+    const r = () => Math.random() * 256 >> 0;
+    return `rgb(${r()}, ${r()}, ${r()})`;
+}
